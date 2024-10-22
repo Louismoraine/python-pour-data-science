b74a4bb468a7b69ca82bad6fcac0023b2827b4e8
louis123 <louis.moraine@eleve.ensai.fr>
2024-10-22 09:42:57 +0000

    geopandas

--- scripts/script1.py
+++ scripts/script1.py
@@ -1,3 +1,23 @@
-print('b')
-print('d')
+import geopandas as gpd
+import contextily as ctx
+import matplotlib.pyplot as plt
 
+from cartiflette import carti_download
+
+communes_borders = carti_download(
+    crs=4326,
+    values=["75", "92", "93", "94"],
+    borders="COMMUNE",
+    vectorfile_format="geojson",
+    filter_by="DEPARTEMENT",
+    source="EXPRESS-COG-CARTO-TERRITOIRE",
+    year=2022,
+)
+communes_borders.sample(3)
+communes_borders.crs
+communes_borders = communes_borders.to_crs(2154)
+communes_92 = communes_borders.loc[communes_borders["INSEE_DEP"]=="92"]
+communes_92.crs
+fig, ax = plt.subplots()
+communes_92.plot()
+plt.show()
